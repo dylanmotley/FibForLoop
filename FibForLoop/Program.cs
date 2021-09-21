0744cafc69d58435b3ecfa68780816097e9510d8
Dyaln Motley <motley.dylan60@gmail.com>
2021-09-21 16:50:37 -0400

    Add project files.

---
+++ FibForLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FibForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num1 = 0;
+            long num2 = 1;
+            long sum = 0;
+            for(long i = 1;i <= 10; i++)
+            {
+                long num3 = num1;
+                num1 = num2;
+                num2 = num3 + num2;
+                Console.WriteLine(num1 + " ");
+
+                if(num1 % 2 == 0)
+                {
+                    sum = sum + num1;
+                }
+            }
+
+            Console.WriteLine("====");
+            Console.WriteLine("Sum of even numbers: "+sum);
+        }
+    }
+}
